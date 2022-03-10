a6288106382bf4d67d682f1759397913167b392d
jpcpereira2001 <jpcpereira@ua.pt>
2022-03-10 21:56:39 +0000

    teste

--- Frontend/Uploads/upload.php
+++ Frontend/Uploads/upload.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../../AdminLTE/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style BruhBruh -->
+  <!-- Theme style -->
   <link rel="stylesheet" href="../../AdminLTE/dist/css/adminlte.min.css">
 </head>
 
